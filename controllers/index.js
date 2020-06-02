3ee53c8c89c3301a5d132b80d29d66a447903ad2
Olonde Felix <olondenarkiso@gmail.com>
2020-06-02 17:39:15 +0300

    added a view

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 module.exports.controller = (app) => {
 // get homepage
 app.get('/', (req, res) => {
-res.render('index', { title: 'I Love Kay' });
+res.render('index', { title: 'Kay-App' });
 })
 }
