2183c0abae9316de21566db25149e861303836ca
Olonde Felix <olondenarkiso@gmail.com>
2020-06-03 17:00:08 +0300

    restful api with express

--- app.js
+++ app.js
@@ -4,21 +4,22 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var fs = require('file-system');
 
 
 
 var app = express();
+//connect to mongodb
 
-// Require file system module
-var fs = require('file-system');
-
-// Include controllers
-fs.readdirSync('controllers').forEach(function (file) {
-if(file.substr(-3) == '.js') {
-const route = require('./controllers/' + file)
-route.controller(app)
-}
-})
+mongoose.connect('mongodb://localhost:27017/mongo_test_queries', {
+    useMongoClient: true
+});
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback) {
+    console.log("Connection Succeeded");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +31,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// include controllers
+fs.readdirSync('controllers').forEach(function(file) {
+    if (file.substr(-3) == '.js') {
+        const route = require('./controllers/' + file)
+        route.controller(app)
+    }
+})
 
 
 
@@ -66,13 +75,14 @@ app.use(function(err, req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-// set locals, only providing error in development
-res.locals.message = err.message;
-res.locals.error = req.app.get('env') === 'development' ? err : {};
-// render the error page
-res.status(err.status || 500);
-res.render('error');
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
 
 app.listen(3000, function() { console.log('listening on 3000') })
 
